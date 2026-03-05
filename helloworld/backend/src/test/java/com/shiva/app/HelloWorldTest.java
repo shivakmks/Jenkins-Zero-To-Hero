406e907fb68243e1462244e85897e0373c99c6c3
shivakmks <shivathudicharla@gmail.com>
2026-03-05 15:29:48 +0530

    Add HelloWorldTest class with a dummy test

---
+++ helloworld/backend/src/test/java/com/shiva/app/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package com.shiva.app;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class HelloWorldTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        // This is a simple "dummy" test that always passes
+        // It's used to verify that the Maven Test lifecycle is working
+        assertTrue(true);
+    }
+}
